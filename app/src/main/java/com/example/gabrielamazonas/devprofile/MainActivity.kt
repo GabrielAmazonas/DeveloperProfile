9d042ba06d7a95357e5593552cf7261f59b79c1e
Gabriel Amazonas <gabriel.amazonas.eng@gmail.com>
2018-01-23 17:36:42 -0200

    First Commit

---
+++ app/src/main/java/com/example/gabrielamazonas/devprofile/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.gabrielamazonas.devprofile
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    //Constraint Layout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
