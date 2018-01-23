4f9780d8206678a0eaae85a4bdd03a867c28bddc
Gabriel Amazonas <gabriel.amazonas.eng@gmail.com>
2018-01-23 18:20:50 -0200

    Adding OkHttpClient Unit Test

--- app/src/test/java/com/example/gabrielamazonas/devprofile/ExampleUnitTest.kt
+++ app/src/test/java/com/example/gabrielamazonas/devprofile/ExampleUnitTest.kt
@@ -1,8 +1,10 @@
 package com.example.gabrielamazonas.devprofile
 
+import okhttp3.*
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.io.IOException
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -12,6 +14,22 @@ import org.junit.Assert.*
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
+
+        fun callApi(url: String) {
+            val client = OkHttpClient()
+            val request = Request.Builder().url(url).build()
+
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call?, e: IOException?) {
+                }
+
+                override fun onResponse(call: Call?, response: Response) {
+                    println(response.body()?.string())
+                }
+            })
+        }
+
+        callApi("https://api.github.com/users/GabrielAmazonas")
         assertEquals(4, 2 + 2)
     }
 }
